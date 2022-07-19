d60b2204cddb1c661632cdc728116736d6b067f5
Nursultan Berdiev <nursultanberdiev1994@gmail.com>
2022-07-19 18:43:09 +0600

    EXAM

---
+++ app/views.py
@@ -0,0 +1,34 @@
+# post functions
+
+def index():
+    pass
+
+
+def employee_create():
+    pass
+
+
+def employee_detail(employee_id):
+    pass
+
+
+def employee_delete(employee_id):
+    pass
+
+
+def employee_update(employee_id):
+    pass
+
+
+# user functions
+
+def register():
+    pass
+
+
+def login():
+    pass
+
+
+def logout():
+    pass
